1352b64f072198abc5e2206d1209d4a849696b1a
Marcin Sulecki <marcin.sulecki@sulmar.pl>
2024-04-09 11:05:39 +0200

    Dodanie udawanej implementacji

---
+++ src/SakilaConsoleApp/Infrastructure/FakeFilmRepository.cs
@@ -0,0 +1,18 @@
+﻿using SakilaConsoleApp.Abstractions;
+using SakilaConsoleApp.Model;
+
+namespace SakilaConsoleApp.Infrastructure
+{
+    internal class FakeFilmRepository : IFilmRepository
+    {
+        public List<Film> GetFilmsAll()
+        {
+            return new List<Film>
+            {
+                new Film { FilmId = 1, Title = "A"},
+                new Film { FilmId = 2, Title = "B"},
+                new Film { FilmId = 3, Title = "C"},
+            };
+        }
+    }
+}
