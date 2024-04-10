5ab9b12dadb87fca442f0e8e945dcd00174cac07
Marcin Sulecki <marcin.sulecki@sulmar.pl>
2024-04-10 11:02:45 +0200

    Dodanie Customer

---
+++ src/SakilaConsoleApp/Model/Customer.cs
@@ -0,0 +1,10 @@
+﻿namespace SakilaConsoleApp.Model
+{
+    public class Customer
+    {
+        public int CustomerId { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Email { get; set; }
+    }
+}
