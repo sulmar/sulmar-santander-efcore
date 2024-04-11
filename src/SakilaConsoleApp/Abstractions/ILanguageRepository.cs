c054bf3990c608ea73e2ca807afffa9e880f3645
Marcin Sulecki <marcin.sulecki@sulmar.pl>
2024-04-11 15:32:44 +0200

    Dodanie języków

---
+++ src/SakilaConsoleApp/Abstractions/ILanguageRepository.cs
@@ -0,0 +1,16 @@
+﻿using SakilaConsoleApp.Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SakilaConsoleApp.Abstractions
+{
+    internal interface ILanguageRepository
+    {
+        void Add(Language language);
+        List<Language> GetAllLanguages();
+        Language GetLanguage(int id);
+    }
+}
---
+++ src/SakilaConsoleApp/Infrastructure/EfDbLanguageRepository.cs
@@ -0,0 +1,37 @@
+﻿using SakilaConsoleApp.Abstractions;
+using SakilaConsoleApp.Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SakilaConsoleApp.Infrastructure
+{
+    internal class EfDbLanguageRepository : ILanguageRepository
+    {
+        private SakilaContext db;
+
+        public EfDbLanguageRepository(SakilaContext db)
+        {
+            this.db = db;
+        }
+
+        public void Add(Language language)
+        {
+            db.Languages.Add(language);
+
+            db.SaveChanges();
+        }
+
+        public List<Language> GetAllLanguages()
+        {
+            return db.Languages.ToList();
+        }
+
+        public Language GetLanguage(int id)
+        {
+            return db.Languages.Find(id);
+        }
+    }
+}
